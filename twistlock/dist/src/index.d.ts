99bc434fc61bdfe7fb81e44235cdc1959a6a4406
minkimcello <minkimcello@gmail.com>
2021-11-02 14:48:10 -0400

    WIP locate mysterious dependencies

--- twistlock/dist/src/index.d.ts
+++ twistlock/dist/src/index.d.ts
@@ -1,5 +1,5 @@
 import { SetupCliReturn, TwistlockResults, TwistlockRun } from "./twistlock";
-export declare function run({ user, password, consoleUrl, project, repositoryPath, octokit }: TwistlockRun): Generator<Generator<any, any, unknown>, void, (SetupCliReturn & TwistlockResults & {
+export declare function run({ user, password, consoleUrl, project, repositoryPath, octokit }: TwistlockRun): Generator<import("@effection/core/dist-esm/operation").OperationPromise<import("@effection/process").ProcessResult> | import("@effection/core/dist-esm/operation").OperationResolution<import("@effection/process").ProcessResult> | import("@effection/core/dist-esm/operation").OperationFuture<import("@effection/process").ProcessResult> | import("@effection/core/dist-esm/operation").OperationFunction<import("@effection/process").ProcessResult> | import("effection").Resource<import("@effection/process").ProcessResult> | Generator<any, any, unknown> | undefined, void, (SetupCliReturn & TwistlockResults & {
     message: any;
     pass: any;
 } & false) | (SetupCliReturn & TwistlockResults & {
