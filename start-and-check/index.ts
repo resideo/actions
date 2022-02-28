2834a8fb8c8f99ad46fa18d845f0c99d645a48bc
Jacob Bolda <me@jacobbolda.com>
2022-02-28 16:21:18 -0600

    start up service and check

---
+++ start-and-check/index.ts
@@ -0,0 +1,32 @@
+import * as core from "@actions/core";
+import { main, withTimeout } from "effection";
+import { daemon } from "@effection/process";
+
+interface CommandExec {
+  command: string;
+  checkForLog: string;
+}
+
+function* run({ command, checkForLog }: CommandExec) {
+  console.log({ command, checkForLog });
+  const myProcess = yield daemon(command);
+
+  yield myProcess.stdout
+    .filter((chunk: any) => chunk.includes(checkForLog))
+    .expect();
+
+  console.log(`command successfully completed${
+    checkForLog !== "" || !checkForLog ? "" : `and included the search text:`
+  }
+  ${checkForLog}`);
+}
+
+main(
+  withTimeout(
+    Number(core.getInput("waitFor")) * 1000,
+    run({
+      command: core.getInput("command"),
+      checkForLog: core.getInput("checkForLog"),
+    })
+  )
+);
