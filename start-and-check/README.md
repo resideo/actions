5710b8749bd59af1b40b4144dd60a38d295900e8
Jacob Bolda <me@jacobbolda.com>
2022-03-01 14:00:29 -0600

    start up service and check (#46)
    * start up service and check
    
    * use volta for the root install
    
    * use org token
    
    * no org token...
    
    * add dist
    
    * remove extra log
    
    * fix search text conditional output
    
    * improving logging visability

---
+++ start-and-check/README.md
@@ -0,0 +1,23 @@
+# Twistlock Action
+
+Run Twistlock to list dependencies that have security vulnerabilities.
+
+## Usage
+```yaml
+on: pull_request
+
+jobs:
+  job_name:
+    name: Job Name
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+    - uses: resideo/actions/twistlock@master
+      with:
+        project: Titan-QA
+        consoleURL: https://twistlock.cloud.resideo.com
+        username: ${{ secrets.TWISTLOCK_USERNAME }}
+        password: ${{ secrets.TWISTLOCK_PASSWORD }}
+      env:
+        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ start-and-check/index.ts
@@ -0,0 +1,61 @@
+import * as core from "@actions/core";
+import { main, withTimeout, spawn, MainError } from "effection";
+import { exec } from "@effection/process";
+
+interface CommandExec {
+  command: string;
+  checkForLog: string;
+}
+
+function* run({ command, checkForLog }: CommandExec) {
+  const startProcess = yield exec(command);
+
+  yield spawn(
+    startProcess.stdout.forEach((text: string) => console.log(text.toString()))
+  );
+
+  yield spawn(
+    startProcess.stderr.forEach((text: string) =>
+      console.error(text.toString())
+    )
+  );
+
+  try {
+    yield startProcess.stdout
+      .filter((chunk: any) => chunk.includes(checkForLog))
+      .expect();
+  } catch (error) {
+    throw new MainError({
+      message: `did not find the stdout text:
+    ${checkForLog}`,
+      exitCode: 1,
+    });
+  }
+
+  console.log(
+    "\x1b[32m%s\x1b[0m",
+    `command successfully completed${
+      checkForLog === undefined || checkForLog === ""
+        ? ""
+        : ` and included the search text:
+  ${checkForLog}`
+    }`
+  );
+}
+
+main(function*() {
+  try {
+    yield withTimeout(
+      Number(core.getInput("waitFor")) * 1000,
+      run({
+        command: core.getInput("command"),
+        checkForLog: core.getInput("checkForLog"),
+      })
+    );
+  } catch (error) {
+    throw new MainError({
+      message: error as string,
+      exitCode: 1,
+    });
+  }
+});
