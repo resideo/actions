f090c8249cd6f1818b21b5d2479f79159cbe3997
Jacob Bolda <me@jacobbolda.com>
2022-03-31 10:54:19 -0500

    more logs around comment posting

--- twistlock/src/githubComment.ts
+++ twistlock/src/githubComment.ts
@@ -8,33 +8,38 @@ export function* postGithubComment(octokit, { message, tag }) {
     const { owner, repo } = github.context.repo;
     const { number } = github.context.payload.pull_request;
 
-    const allComments = yield octokit.rest.issues.listComments({
-      owner,
-      repo,
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      issue_number: number,
-    });
-
-    const previousComment =
-      allComments.data.length > 0 &&
-      allComments.data.find((comment) => comment.body.includes(tag));
-
-    if (previousComment) {
-      yield octokit.rest.issues.updateComment({
-        owner,
-        repo,
-        // eslint-disable-next-line @typescript-eslint/camelcase
-        comment_id: previousComment.id,
-        body: `${message}`,
-      });
-    } else {
-      yield octokit.rest.issues.createComment({
+    try {
+      const allComments = yield octokit.rest.issues.listComments({
         owner,
         repo,
         // eslint-disable-next-line @typescript-eslint/camelcase
         issue_number: number,
-        body: `${message}`,
       });
+
+      const previousComment =
+        allComments.data.length > 0 &&
+        allComments.data.find((comment) => comment.body.includes(tag));
+
+      if (previousComment) {
+        yield octokit.rest.issues.updateComment({
+          owner,
+          repo,
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          comment_id: previousComment.id,
+          body: `${message}`,
+        });
+      } else {
+        yield octokit.rest.issues.createComment({
+          owner,
+          repo,
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          issue_number: number,
+          body: `${message}`,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      core.setFailed(error as string);
     }
   }
 }
